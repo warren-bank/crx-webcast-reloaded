7fc161fc0f3e178f9600ae3e3b4a75e909736027
Warren R Bank <warren.r.bank@gmail.com>
2018-08-02 00:17:44 -0700

    minor modifications made while attempting to debug an issue:
    when URL hash is parsed at page load and is found to contain encoded URLs,
    the call from "initialize_videoplayer" to "connect_chromecast" has no effect.
    
    this issue has not been resolved, and it's really frustrating..
    
    it has no effect:
    if "connect_chromecast" is scheduled to be called from "initialize_videoplayer":
    * in a timer
    * in an event handler
    * invoked immediately
    
    it works as expected:
    when "connect_chromecast" is called from any other context
    * the javascript console
    * the input form button's onclick handler

--- external_website/js/webcast-reloaded.js
+++ external_website/js/webcast-reloaded.js
@@ -1,33 +1,62 @@
 // --------------------------------------------------------- video player managment tasks:
 
-var connect_chromecast = function() {
-    THEOplayer.cast.chromecast.initialize()
-    .then(function(){
-        THEOplayer.cast.chromecast.startSession()
-    })
+var connect_chromecast = function(player) {
+    if (player && player.cast && !player.cast.casting && player.cast.chromecast && !player.cast.chromecast.casting) {
+        if (player.cast.chromecast.state.toLowerCase() === 'unavailable') {
+            var $statechangeEventListener = function() {
+                if (player && player.cast && player.cast.chromecast) {
+                    if (player.cast.casting || player.cast.chromecast.casting) {
+                        player.cast.chromecast.removeEventListener('statechange', $statechangeEventListener)
+                    }
+                    else if (player.cast.chromecast.state.toLowerCase() === 'available') {
+                        player.cast.chromecast.removeEventListener('statechange', $statechangeEventListener)
+                        player.cast.chromecast.start()
+                    }
+                }
+            }
+            player.cast.chromecast.addEventListener('statechange', $statechangeEventListener)
+        }
+        else {
+            player.cast.chromecast.start()
+        }
+    }
+    else {
+        THEOplayer.cast.chromecast.initialize()
+        .then(function(){
+            THEOplayer.cast.chromecast.startSession()
+        })
+    }
 }
 
-var disconnect_chromecast = function() {
-    THEOplayer.cast.chromecast.endSession()
+var disconnect_chromecast = function(player) {
+    if (player && player.cast && player.cast.casting && player.cast.chromecast && player.cast.chromecast.casting) {
+        player.cast.chromecast.stop()
+    }
+    else {
+        THEOplayer.cast.chromecast.endSession()
+    }
 }
 
 var webcast_video_player
 
-var destroy_videoplayer = function(skip_reload) {
+var destroy_videoplayer = function(skip_cc, skip_reload) {
     if (webcast_video_player) {
         webcast_video_player.stop()
         webcast_video_player.destroy()
-        disconnect_chromecast()
 
-        history.replaceState("", document.title, window.location.pathname)
+        if (skip_cc !== true) {
+            disconnect_chromecast()
+        }
+
         if (skip_reload !== true) {
+            history.replaceState("", document.title, window.location.pathname)
             window.location.reload()
         }
     }
 }
 
 var initialize_videoplayer = function(URL_video, URL_subtitle) {
-    if (! URL_video) return;
+    if (! URL_video) return
 
     var settings = { 
         sources: [{
@@ -47,7 +76,25 @@ var initialize_videoplayer = function(URL_video, URL_subtitle) {
         delete settings.textTracks
     }
 
-    destroy_videoplayer(true)
+    var types = [
+        {ext: "m3u8", mime: "application/x-mpegURL"},
+        {ext: "mpd",  mime: "application/dash+xml"},
+        {ext: "mp4",  mime: "video/mp4"},
+        {ext: "webm", mime: "video/webm"}
+    ]
+    types.every(function(type){
+        var pattern = new RegExp(('\\.' + type.ext + '(?:[#\?]|$)'), 'i')
+
+        if (pattern.test(URL_video)) {
+            settings.sources[0].type = type.mime
+            return false
+        }
+        return true
+    })
+    types = undefined
+
+    // cleanup
+    destroy_videoplayer(true, true)
 
     webcast_video_player = new THEOplayer.Player(
         document.querySelector('#video-player'),
@@ -63,6 +110,11 @@ var initialize_videoplayer = function(URL_video, URL_subtitle) {
     webcast_video_player.autoplay = false
     webcast_video_player.source   = settings
 
+    // open the Chromecast connection dialog
+    connect_chromecast()
+//  connect_chromecast(webcast_video_player)
+
+    // display direct links to the video stream and subtitle sources
     var info
     info = `now playing: <a href="${URL_video}">video</a>`
     if (URL_subtitle) {
@@ -72,8 +124,6 @@ var initialize_videoplayer = function(URL_video, URL_subtitle) {
     document.querySelector('.webcast-video-player .video-player-info .video-player-info-status').innerHTML = info
 
     document.querySelector('.webcast-video-player .video-player-info i.material-icons').onclick = destroy_videoplayer
-
-    connect_chromecast()
 }
 
 // --------------------------------------------------------- webpage initialization:
@@ -88,11 +138,11 @@ var show_player = function() {
 }
 
 var parse_location_hash = function() {
-    var b64, hash_regex_pattern, URL_video, URL_subtitle;
+    var b64, hash_regex_pattern, URL_video, URL_subtitle
 
-    b64 = '[A-Za-z0-9+/=%]';
-    hash_regex_pattern = `^#/watch/(${b64}+?)(?:/subtitle/(${b64}+))?$`;
-    hash_regex_pattern = new RegExp(hash_regex_pattern);
+    b64 = '[A-Za-z0-9+/=%]'
+    hash_regex_pattern = `^#/watch/(${b64}+?)(?:/subtitle/(${b64}+))?$`
+    hash_regex_pattern = new RegExp(hash_regex_pattern)
 
     var decode_URL = function(str) {
       var tail, done
@@ -110,9 +160,9 @@ var parse_location_hash = function() {
 
     var matches = hash_regex_pattern.exec(window.location.hash)
     if (matches && matches.length && matches[1]) {
-        URL_video = decode_URL( matches[1] );
+        URL_video = decode_URL( matches[1] )
         if (matches.length > 2 && matches[2]) {
-            URL_subtitle = decode_URL( matches[2] );
+            URL_subtitle = decode_URL( matches[2] )
         }
     }
     return {URL_video, URL_subtitle}
@@ -127,12 +177,12 @@ var $DOMContentLoaded = function () {
     }
     else {
         document.querySelector('.URL-entry-form button').onclick = function() {
-            URL_video    = document.querySelector('.URL-entry-form input#URL_video').value;
-            URL_subtitle = document.querySelector('.URL-entry-form input#URL_subtitle').value;
+            URL_video    = document.querySelector('.URL-entry-form input#URL_video').value
+            URL_subtitle = document.querySelector('.URL-entry-form input#URL_subtitle').value
 
             if (URL_video) {
-                show_player();
-                initialize_videoplayer(URL_video, URL_subtitle);
+                show_player()
+                initialize_videoplayer(URL_video, URL_subtitle)
             }
         }
     }
@@ -155,5 +205,5 @@ document.addEventListener("DOMContentLoaded", $DOMContentLoaded)
 window.addEventListener("hashchange", $hashchange, false)
 
 window.onunload = function() {
-    destroy_videoplayer(true)
+    destroy_videoplayer(false, true)
 }
