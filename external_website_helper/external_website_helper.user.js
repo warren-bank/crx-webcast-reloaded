28afbc0a04f3f81bca746d2c588b810a3890022f
Warren R Bank <warren.r.bank@gmail.com>
2021-07-19 01:17:46 -0700

    update greasemonkey userscript

--- external_website_helper/external_website_helper.user.js
+++ external_website_helper/external_website_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WebCast-Reloaded Helper
 // @description  Attempts to workaround issue #1 by automatically redirecting video between secure and insecure external website hosts depending upon the desired behavior.
-// @version      0.2.0
+// @version      0.3.0
 // @match        *://warren-bank.github.io/crx-webcast-reloaded/external_website/*
 // @match        *://webcast-reloaded.surge.sh/*
 // @match        *://gitcdn.link/cdn/warren-bank/crx-webcast-reloaded/gh-pages/external_website/*
@@ -26,6 +26,11 @@ var user_options = {
       "script_enabled":             true,
       "prioritize_cast_over_watch": true
     },
+    "prioritize_script_language": {
+      "script_enabled":             true,
+      "redirect_to_es5":            false,
+      "redirect_to_es6":            false
+    },
     "prepopulate_incognito_forms": {
       "script_enabled":             true,
       "airplay_sender": {
@@ -43,26 +48,32 @@ var user_options = {
 }
 
 // -----------------------------------------------------------------------------
+// conditionally redirect HTTP protocol
 
 var workaround_issue_01 = function(){
 
   // ===========================================================================
 
-  const is_tls_endpoint = () => window.location.protocol.trim().toLowerCase().startsWith('https')
+  var is_tls_endpoint = function(){
+    return window.location.protocol.trim().toLowerCase().startsWith('https')
+  }
 
-  const get_endpoint = () => {
-    const endpoint = {
-      chromecast_sender: false,
-      airplay_sender:    false,
-      proxy:             false
+  var get_endpoint = function(){
+    var endpoint = {
+      chromecast_sender:  false,
+      airplay_sender:     false,
+      airplay_sender_es5: false,
+      proxy:              false
     }
 
-    const pathname = window.location.pathname.trim().toLowerCase()
+    var pathname = window.location.pathname.trim().toLowerCase()
 
     if (pathname.endsWith('chromecast_sender.html'))
       endpoint.chromecast_sender = true
     else if (pathname.endsWith('airplay_sender.html'))
       endpoint.airplay_sender = true
+    else if (pathname.endsWith('airplay_sender.es5.html'))
+      endpoint.airplay_sender_es5 = true
     else if (pathname.endsWith('proxy.html'))
       endpoint.proxy = true
 
@@ -71,16 +82,16 @@ var workaround_issue_01 = function(){
 
   // ===========================================================================
 
-  const redirect = (endpoint, is_user_initiated) => {
-    const override_search = '?override=true'
+  var redirect = function(endpoint, is_user_initiated){
+    var override_search = '?override=true'
 
     // short-circuit automatic redirects when the user initiated the redirect that brought them to this page
     if (!is_user_initiated && (window.location.search === override_search))
       return
 
-    let webcast_reloaded_base, url
+    var webcast_reloaded_base, url
 
-    const to_protocol = is_tls_endpoint() ? 'http' : 'https'
+    var to_protocol = is_tls_endpoint() ? 'http' : 'https'
 
     webcast_reloaded_base = {
       "https": "https://warren-bank.github.io/crx-webcast-reloaded/external_website/",
@@ -93,6 +104,8 @@ var workaround_issue_01 = function(){
       url = 'chromecast_sender.html'
     else if (endpoint.airplay_sender)
       url = 'airplay_sender.html'
+    else if (endpoint.airplay_sender_es5)
+      url = 'airplay_sender.es5.html'
     else if (endpoint.proxy)
       url = 'proxy.html'
 
@@ -106,7 +119,7 @@ var workaround_issue_01 = function(){
 
   // ===========================================================================
 
-  const decode_URL = (str) => {
+  var decode_URL = function(str){
     var tail, done
 
     if (str) {
@@ -120,11 +133,11 @@ var workaround_issue_01 = function(){
     return str
   }
 
-  const get_video_url = () => {
-    let video_url, b64, hash_regex_pattern, matches
+  var get_video_url = function(){
+    var video_url, b64, hash_regex_pattern, matches
 
     b64 = '[A-Za-z0-9+/=%]'
-    hash_regex_pattern = `^#/watch/(${b64}+?)(?:/subtitle/(${b64}+?))?(?:/referer/(${b64}+?))?$`
+    hash_regex_pattern = '^#/watch/(' + b64 + '+?)(?:/subtitle/(' + b64 + '+?))?(?:/referer/(' + b64 + '+?))?$'
     hash_regex_pattern = new RegExp(hash_regex_pattern)
 
     matches = hash_regex_pattern.exec(window.location.hash)
@@ -136,7 +149,7 @@ var workaround_issue_01 = function(){
     return video_url
   }
 
-  const is_tls_video_stream = (video_url) => {
+  var is_tls_video_stream = function(video_url){
     if (!video_url)
       video_url = get_video_url()
 
@@ -145,10 +158,10 @@ var workaround_issue_01 = function(){
       : (video_url.trim().substring(0,5).toLowerCase() === 'https')
   }
 
-  const get_chrome_major_version = () => {
-    const useragent = navigator.userAgent
-    const regex     = /^.*\bChrome\/(\d+)\..*$/
-    let version
+  var get_chrome_major_version = function(){
+    var useragent = navigator.userAgent
+    var regex     = /^.*\bChrome\/(\d+)\..*$/
+    var version
 
     version = useragent.replace(regex, '$1')
     version = Number(version)
@@ -156,21 +169,20 @@ var workaround_issue_01 = function(){
     return isNaN(version) ? 0 : version
   }
 
-  const update_chromecast_sender_DOM = (endpoint, message_text, button_text) => {
-    const button_id = 'redirect_button'
-    const $div      = document.createElement('div')
+  var update_chromecast_sender_DOM = function(endpoint, message_text, button_text){
+    var button_id = 'redirect_button'
+    var $div      = document.createElement('div')
 
-    $div.innerHTML = `
-      <hr />
-      <div>${message_text}</div>
-      <button id="${button_id}">${button_text}</button>
-    `
+    $div.innerHTML = ''
+      + '<hr />'
+      + '<div>' + message_text + '</div>'
+      + '<button id="' + button_id + '">' + button_text + '</button>'
 
     document.body.appendChild($div)
 
-    const $button = document.getElementById(button_id)
+    var $button = document.getElementById(button_id)
 
-    $button.addEventListener('click', (event) => {
+    $button.addEventListener('click', function(event){
       event.preventDefault()
       event.stopPropagation()
 
@@ -178,13 +190,13 @@ var workaround_issue_01 = function(){
     })
   }
 
-  const process_chromecast_sender = (endpoint) => {
+  var process_chromecast_sender = function(endpoint){
     if (!window.location.hash)
       return
 
-    const tls_endpoint         = is_tls_endpoint()
-    const tls_video_stream     = is_tls_video_stream()
-    const chrome_major_version = get_chrome_major_version()
+    var tls_endpoint         = is_tls_endpoint()
+    var tls_video_stream     = is_tls_video_stream()
+    var chrome_major_version = get_chrome_major_version()
 
     if (tls_video_stream && tls_endpoint) {
       // cast:  YES
@@ -246,12 +258,13 @@ var workaround_issue_01 = function(){
 
   // ===========================================================================
 
-  const process_page = () => {
-    const tls_endpoint = is_tls_endpoint()
-    const     endpoint = get_endpoint()
+  var process_page = function(){
+    var tls_endpoint   = is_tls_endpoint()
+    var endpoint       = get_endpoint()
+    var airplay_sender = endpoint.airplay_sender || endpoint.airplay_sender_es5
 
     // airplay_sender => HTTP only
-    if (endpoint.airplay_sender && tls_endpoint) {
+    if (airplay_sender && tls_endpoint) {
       redirect(endpoint)
       return
     }
@@ -270,21 +283,72 @@ var workaround_issue_01 = function(){
 }
 
 // ----------------------------------------------------------------------------- </workaround_issue_01>
+// conditionally redirect HTTP pathname
+
+var prioritize_script_language = function(){
+
+  // ===========================================================================
+
+  var get_endpoint = function(){
+    var endpoint = {
+      airplay_sender:     false,
+      airplay_sender_es5: false
+    }
+
+    var pathname = window.location.pathname.trim().toLowerCase()
+
+    if (pathname.endsWith('airplay_sender.html'))
+      endpoint.airplay_sender = true
+    else if (pathname.endsWith('airplay_sender.es5.html'))
+      endpoint.airplay_sender_es5 = true
+
+    return endpoint
+  }
+
+  // ===========================================================================
+
+  var process_page = function(){
+    var endpoint = get_endpoint()
+    var config   = window.webcast_reloaded_external_website_helper.prioritize_script_language
+    var url      = window.location.href
+
+    // airplay_sender: es6 => es5
+    if (endpoint.airplay_sender && config.redirect_to_es5) {
+      window.location = url.replace(/airplay_sender\.html/i, 'airplay_sender.es5.html')
+      return
+    }
+
+    // airplay_sender: es5 => es6
+    if (endpoint.airplay_sender_es5 && config.redirect_to_es6) {
+      window.location = url.replace(/airplay_sender\.es5\.html/i, 'airplay_sender.html')
+      return
+    }
+  }
+
+  process_page()
+}
+
+// ----------------------------------------------------------------------------- </prioritize_script_language>
+// prepopulate form fields in incognito with preconfigured values,
+// since form fields in normal windows are prepopulated from persistent cookies
 
 var prepopulate_incognito_forms = function(){
 
   // ===========================================================================
 
-  const get_endpoint = () => {
-    const endpoint = {
-      airplay_sender: false,
-      proxy:          false
+  var get_endpoint = function(){
+    var endpoint = {
+      airplay_sender:     false,
+      airplay_sender_es5: false,
+      proxy:              false
     }
 
-    const pathname = window.location.pathname.trim().toLowerCase()
+    var pathname = window.location.pathname.trim().toLowerCase()
 
     if (pathname.endsWith('airplay_sender.html'))
       endpoint.airplay_sender = true
+    else if (pathname.endsWith('airplay_sender.es5.html'))
+      endpoint.airplay_sender_es5 = true
     else if (pathname.endsWith('proxy.html'))
       endpoint.proxy = true
 
@@ -293,30 +357,30 @@ var prepopulate_incognito_forms = function(){
 
   // ===========================================================================
 
-  const update_form_field_textbox = (id, val) => {
-    const $field = document.getElementById(id)
+  var update_form_field_textbox = function(id, val){
+    var $field = document.getElementById(id)
 
     if ($field)
       $field.value = val
   }
 
-  const update_form_field_checkbox = (id, val) => {
+  var update_form_field_checkbox = function(id, val){
     if (!val)
       return
 
-    const $field = document.getElementById(id)
+    var $field = document.getElementById(id)
 
     if ($field)
       $field.checked = true
   }
 
-  const process_airplay_sender = (configs) => {
+  var process_airplay_sender = function(configs){
     update_form_field_textbox( 'airplay_host', configs.host)
     update_form_field_textbox( 'airplay_port', configs.port)
     update_form_field_checkbox('airplay_tls',  configs.tls)
   }
 
-  const process_proxy = (configs) => {
+  var process_proxy = function(configs){
     update_form_field_textbox( 'host', configs.host)
     update_form_field_textbox( 'port', configs.port)
     update_form_field_checkbox('tls',  configs.tls)
@@ -324,10 +388,11 @@ var prepopulate_incognito_forms = function(){
 
   // ===========================================================================
 
-  const process_page = () => {
-    const endpoint = get_endpoint()
+  var process_page = function(){
+    var endpoint       = get_endpoint()
+    var airplay_sender = endpoint.airplay_sender || endpoint.airplay_sender_es5
 
-    if (endpoint.airplay_sender) {
+    if (airplay_sender) {
       process_airplay_sender(window.webcast_reloaded_external_website_helper.prepopulate_incognito_forms.airplay_sender)
       return
     }
@@ -338,13 +403,13 @@ var prepopulate_incognito_forms = function(){
     }
   }
 
-  const process_page_in_incognito_window = () => {
-    const fs = window.RequestFileSystem || window.webkitRequestFileSystem
+  var process_page_in_incognito_window = function(){
+    var fs = window.RequestFileSystem || window.webkitRequestFileSystem
 
     if (!fs)
       return
 
-    const callback = (is_incognito) => {
+    var callback = function(is_incognito){
       if (is_incognito)
         process_page()
     }
@@ -390,9 +455,11 @@ var inject_function = function(_function){
 }
 
 var inject_options = function(){
-  var _function = `function(){
-    window.webcast_reloaded_external_website_helper = ${JSON.stringify(user_options['webcast_reloaded_external_website_helper'])}
-  }`
+  var _function = ''
+    + 'function(){'
+    + '  window.webcast_reloaded_external_website_helper = ' + JSON.stringify(user_options['webcast_reloaded_external_website_helper'])
+    + '}'
+
   inject_function(_function)
 }
 
@@ -401,6 +468,9 @@ var bootstrap = function(){
 
   if (user_options.webcast_reloaded_external_website_helper.workaround_issue_01.script_enabled)
     inject_function(workaround_issue_01)
+
+  if (user_options.webcast_reloaded_external_website_helper.prioritize_script_language.script_enabled)
+    inject_function(prioritize_script_language)
 
   if (user_options.webcast_reloaded_external_website_helper.prepopulate_incognito_forms.script_enabled)
     inject_function(prepopulate_incognito_forms)
